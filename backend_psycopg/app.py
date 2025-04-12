506a2a036bc9c123fbee7bbdc74f5aff8adae604
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-04-12 14:34:10 -0300

    fix: add compose down in case of closing the app

--- backend_psycopg/app.py
+++ backend_psycopg/app.py
@@ -1,11 +1,19 @@
 import subprocess
+import time
+import psycopg
 from app import create_app, wait_for_db
 
 if __name__ == "__main__":
-    print("Starting Docker Compose...")
-    subprocess.run(["docker", "compose", "up", "-d"], check=True)
+    try:
+        print("Starting Docker Compose...")
+        subprocess.run(["docker", "compose", "up", "-d"], check=True)
 
-    wait_for_db()
+        wait_for_db()
 
-    app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=3000)
+        app = create_app()
+        app.run(debug=True, host="0.0.0.0", port=3000)
+    except KeyboardInterrupt:
+        print("Application interrupted by user.")
+    finally:
+        print("Stopping Docker Compose...")
+        subprocess.run(["docker", "compose", "down"], check=True)
