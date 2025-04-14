862d20c4cbaba254dbbdc5d27309293a283e4a37
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-04-14 17:36:45 -0300

    fix: add total_amount

--- backend_psycopg/app/controllers/employee_controller.py
+++ backend_psycopg/app/controllers/employee_controller.py
@@ -34,15 +34,42 @@ class EmployeeController:
                 or []
             )
 
+            emp_orders_details = (
+                self.order_dao.get_total_orders_by_employee_id(
+                    emp.employeeid, start_date, end_date
+                )
+                or []
+            )
+
+            emp_order_details: List[OrderDetails] = []
+            for order in emp_orders_details:
+                details = self.order_details_dao.get_order_details_by_order_id(
+                    order.orderid
+                )
+                if details:
+                    if isinstance(details, list):
+                        emp_order_details.extend(details)
+                    else:
+                        emp_order_details.append(details)
+
+            emp_total_amount = 0
+            for detail in emp_order_details:
+                qty = detail.quantity if detail.quantity is not None else 0
+                price = detail.unitprice if detail.unitprice is not None else 0
+                discount = detail.discount if detail.discount is not None else 0
+                emp_total_amount += qty * price * (1 - discount)
+
             ranking_list.append(
                 {
                     "employee_id": emp.employeeid,
                     "employee_name": f"{emp.firstname} {emp.lastname}",
                     "total_orders": len(emp_orders),
+                    "total_orders_details": len(emp_order_details),
+                    "total_amount": emp_total_amount,
                 }
             )
 
-        ranking_list.sort(key=lambda x: x["total_orders"], reverse=True)
+        ranking_list.sort(key=lambda x: float(x["total_orders"]), reverse=True)
 
         for index, employee in enumerate(ranking_list, start=1):
             employee["rank"] = index
